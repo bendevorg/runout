9e8e6d22217cc0268b299729af57db54a18ba3a6
GuiMac <guibasconti@gmail.com>
2019-05-20 01:00:35 -0300

    Add movement to fighter

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
 public class EnemyMovement : MonoBehaviour {
 
   public float rotationSpeed = 0.1f;
   public float movementSpeed = 3f;
   CharacterController characterController;
 
+  Animator animator;
+
   void Start() {
     characterController = this.GetComponent<CharacterController>();
+    animator = this.GetComponent<Animator>();
   }
 
   public void Move(Vector3 moveDirection) {
     characterController.Move(moveDirection.normalized * Time.deltaTime * movementSpeed);
+    Animate(moveDirection.x, Mathf.Abs(moveDirection.z));
 	}
 
   public void Rotate(Vector3 target) {
@@ -24,5 +29,10 @@ public class EnemyMovement : MonoBehaviour {
     //   rotationSpeed
     // );
     transform.LookAt(target, Vector3.up);
+  }
+
+  private void Animate(float inputX, float inputY) {
+    animator.SetFloat("SpeedX", inputX);
+    animator.SetFloat("SpeedY", inputY);
   }
 }
