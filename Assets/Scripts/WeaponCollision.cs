5f67886ab5ce63e0438bcd64543c68c39770c2fa
GuiMac <guibasconti@gmail.com>
2019-06-06 21:44:00 -0300

    Enemy hit detection

---
+++ Assets/Scripts/WeaponCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollision : MonoBehaviour {
+
+  public LayerMask layerMask;
+  public float damage = 15f;
+
+  public Vector3 hitPoint;
+
+  private void OnTriggerEnter(Collider other) {
+    if (other.CompareTag("Player")) {
+      Ray ray = new Ray(transform.position, -transform.forward);
+      RaycastHit hit;
+      if (Physics.Raycast(
+          transform.position + (transform.forward * 1f), -transform.forward,
+          out hit,
+          4f,
+          layerMask
+        )) {
+        Debug.Log("Hit");
+        hitPoint = hit.point;
+      }
+    }
+  }
+
+  private void OnDrawGizmos() {
+    Gizmos.color = Color.red;
+    Ray ray = new Ray(transform.position + (transform.forward * 5f), -transform.forward);
+    Gizmos.DrawRay(ray);
+
+    Gizmos.DrawSphere(hitPoint, .2f);
+  }
+}
