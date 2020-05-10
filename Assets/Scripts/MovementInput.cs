600cf3e288bdaa7d2676bf2fab8b7db94a89591b
GuiMac <guibasconti@gmail.com>
2020-05-09 23:43:02 -0300

    Add Jump

--- Assets/Scripts/MovementInput.cs
+++ Assets/Scripts/MovementInput.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 // [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class MovementInput : MonoBehaviour {
 
   private float inputX;
   private float inputZ;
+  private bool jump;
   Vector3 moveDirection;
 
   public float rotationSpeed = 0.1f;
   public int movementSpeed = 5;
+  public int jumpForce = 5;
 
   public Camera playerCamera;
 
   private Animator animator;
   private Rigidbody rb;
   private LivingEntity livingEntity;
+  private CapsuleCollider collider;
 
   [HideInInspector]
   public bool canMove = true;
@@ -33,6 +37,7 @@ public class MovementInput : MonoBehaviour {
     rb = this.GetComponent<Rigidbody>();
     livingEntity = this.GetComponent<LivingEntity>();
     livingEntity.OnHit += Knockback;
+    collider = this.GetComponent<CapsuleCollider>();
   }
 
   void Update() {
@@ -53,6 +58,17 @@ public class MovementInput : MonoBehaviour {
   private void ReadInput() {
     inputX = Input.GetAxis("Horizontal");
     inputZ = Input.GetAxis("Vertical");
+    jump = Input.GetKeyDown(KeyCode.Space);
+  }
+
+  private bool IsGrounded() {
+    return Physics.Raycast(transform.position, -Vector3.up, 0.1f);
+  }
+
+  private void OnDrawGizmos() {
+    Gizmos.color = Color.red;
+    Ray ray = new Ray(transform.position, -Vector3.up * 0.1f);
+    Gizmos.DrawRay(ray);
   }
 
   private void CalculateMovementIntent() {
@@ -64,6 +80,10 @@ public class MovementInput : MonoBehaviour {
     right.y = 0;
 
     moveDirection = forward.normalized * inputZ + right.normalized * inputX;
+    if (jump && IsGrounded()) {
+      animator.SetTrigger("Jump");
+      Jump();
+    }
   }
 
   private void Move() {
@@ -76,6 +96,10 @@ public class MovementInput : MonoBehaviour {
       new Quaternion(0, playerCamera.transform.rotation.y, 0, playerCamera.transform.rotation.w),
       rotationSpeed
     );
+  }
+
+  private void Jump() {
+    rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
   }
 
   public void Knockback(float damage, float weight, Vector3 hitPoint, Vector3 hitDirection) {
